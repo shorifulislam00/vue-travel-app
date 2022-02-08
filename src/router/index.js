725f673058baab1eccd095bb13eb16226aece922
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-08 23:29:52 +0600

    lazy load routing done

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,27 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/brazil",
+    name: "Brazil",
+    component: ()  => import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: ()  => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: ()  => import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: ()  => import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
   },
 ];
 
