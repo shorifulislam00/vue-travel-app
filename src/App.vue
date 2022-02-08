09f2a0b57e0105a289f4dec77170f17f4b8c4167
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-08 13:13:03 +0600

    router basic testing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <a href="/">a tag</a> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
