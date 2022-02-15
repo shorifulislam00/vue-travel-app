3112d40afbe229db32a7c1860ac1ca2fcace7a06
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-15 17:24:15 +0600

    vue router active class lession done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/brazil">Brazil</router-link> |
-      <router-link to="/hawaii">Hawaii</router-link> |
-      <router-link to="/jamaica">Jamaica</router-link> |
-      <router-link to="/panama">Panama</router-link>
+      <TheNavigation />
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import TheNavigation from "@/components/TheNavigation"
+
+export default {
+  components: {
+    TheNavigation
+  }
+}
+</script>
 
 <style>
 #app {
@@ -20,17 +26,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  padding: 0 10px;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "vue-school-active-class",
   routes,
 });
 
