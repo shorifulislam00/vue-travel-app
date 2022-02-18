d1a31a0ca5bd017bd9158accd6fe869080c0b41b
Md Shoriful Islam <shorifulislam433@gmail.com>
2022-02-18 16:50:32 +0600

    name route, params, dynamic route, re-render component lession done

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,31 @@ const routes = [
     name: "Home",
     component: Home
   },
+  // {
+  //   path: "/brazil",
+  //   name: "Brazil",
+  //   component: ()  => import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
+  // },
+  // {
+  //   path: "/hawaii",
+  //   name: "Hawaii",
+  //   component: ()  => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
+  // },
+  // {
+  //   path: "/jamaica",
+  //   name: "Jamaica",
+  //   component: ()  => import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
+  // },
+  // {
+  //   path: "/panama",
+  //   name: "Panama",
+  //   component: ()  => import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
+  // },
   {
-    path: "/brazil",
-    name: "Brazil",
-    component: ()  => import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue")
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: ()  => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue")
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: ()  => import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue")
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: ()  => import(/* webpackChunkName: "panama" */ "../views/Panama.vue")
-  },
+    path: "/details/:id",
+    name: "DestinationDetails",
+    component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails")
+  }
 ];
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,12 @@
 
     <div class="destinations">
       <div v-for="destination in destinations" :key="destination.name">
-          <router-link :to="destination.slug">
+          <router-link :to="{ name: 'DestinationDetails', params: {id: destination.id} }">
               <h2>{{ destination.name }}</h2>
           </router-link>
 
           <figure>
-            <router-link :to="destination.slug">
+            <router-link :to="{ name: 'DestinationDetails', params: {id: destination.id} }">
               <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name" />
             </router-link>
           </figure>
@@ -46,6 +46,16 @@ export default {
   .destinations{
     display: flex;
     justify-content: space-between;
+  }
+
+  a {
+    color: lightseagreen;
+    text-decoration: none;
+  }
+
+  a:hover,
+  a:visited{
+    text-decoration: underline;
   }
 
 </style>
